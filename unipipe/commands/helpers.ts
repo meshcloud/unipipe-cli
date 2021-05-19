96e2c1e2dc5988e7a203dd45e97875b5a7286b4b
Johannes Rudolph <jrudolph@meshcloud.io>
2021-05-19 20:13:37 +0200

    doc: add a todo note to EnumType

--- unipipe/commands/helpers.ts
+++ unipipe/commands/helpers.ts
@@ -1,21 +1,21 @@
 import { ITypeInfo, path, Type } from '../deps.ts';
-import { readInstance, ServiceInstance, readBinding, ServiceBinding } from '../osb.ts';
+import { readBinding, readInstance, ServiceBinding, ServiceInstance } from '../osb.ts';
 
 // TODO it would be nice to generate help text for allowed enum values
+// TODO replace with official onec available https://github.com/c4spar/deno-cliffy/tree/main/command#enum-option-type
 export class EnumType extends Type<string> {
-  
-  constructor(private readonly allowed: readonly string[]){
+  constructor(private readonly allowed: readonly string[]) {
     super();
-}
+  }
 
   public parse({ label, name, value }: ITypeInfo): string {
     if (!this.allowed.includes(value)) {
-      const legal = this.allowed.map(x => `"${x}"`).join(", ");
+      const legal = this.allowed.map((x) => `"${x}"`).join(", ");
 
       console.error(
         `Illegal ${label.toLowerCase()} ${name}: must be one of [${legal}] but got "${value}".`,
       );
-      Deno.exit(1)
+      Deno.exit(1);
     }
 
     return value;
@@ -39,7 +39,7 @@ export async function mapInstances<T>(
   mapFn: (serviceInstance: ServiceInstance) => Promise<T>,
   filterFn: (serviceInstance: ServiceInstance) => boolean = (instance: ServiceInstance) => {
     return true
-    },
+  }
 ): Promise<T[]> {
   const instancesPath = path.join(osbRepoPath, "instances");
   const results: T[] = [];
@@ -51,7 +51,7 @@ export async function mapInstances<T>(
       }
 
       const ip = path.join(instancesPath, dir.name);
-      
+
       try {
         const instance = await readInstance(ip);
 
@@ -64,23 +64,31 @@ export async function mapInstances<T>(
           const r = await mapFn(instance);
           results.push(r);
         } catch (error) {
-          console.error(`Failed to process service instance "${ip}".\n`, error);
+          console.error(
+            `Failed to process service instance "${ip}".\n`,
+             error
+          );
           Deno.exit(1);
         }
 
       } catch (error) {
-        console.error(`Failed to apply filter to service instance "${ip}".\n`, error);
+        console.error(
+          `Failed to apply filter to service instance "${ip}".\n`,
+          error,
+        );
         Deno.exit(1);
       }
     }
   } catch (error) {
-    console.error(`Failed to read instances directory "${instancesPath}".\n`, error);
+    console.error(
+      `Failed to read instances directory "${instancesPath}".\n`,
+      error,
+    );
     Deno.exit(1);
   }
 
   return results;
 }
-
 
 /**
  * A helper function to implement commands that need to perform a map operation on bindings.
