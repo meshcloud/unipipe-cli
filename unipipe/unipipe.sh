5aa1d0e689b13d1361cc3c245d6b97725d0b6b26
Felix Zieger <fzieger@meshcloud.io>
2021-10-26 12:33:34 +0200

    docs: closing commit

--- unipipe/unipipe.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# Exit on all errors and undefined vars
-set -o errexit
-set -o nounset
-
-deno run --allow-read --allow-write --allow-env --unstable "$(dirname "$0")"/main.ts "$@"
